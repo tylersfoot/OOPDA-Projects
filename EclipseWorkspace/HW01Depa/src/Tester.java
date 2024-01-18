1e7be60ed19e5bed288d8bdb19636e2950ac1413
tylersfoot <tylerdepa8@gmail.com>
2024-01-17 19:36:57 -0500

    HW01

---
+++ EclipseWorkspace/HW01Depa/src/Tester.java
@@ -0,0 +1,18 @@
+
+public class Tester {
+
+	public static void main(String[] args) {
+		int num1, num2, sum;
+		double average;
+		
+		num1 = 16;
+		num2 = 12;
+		sum = num1 + num2;
+		average = sum / 2.0;
+		
+		System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+		System.out.println("The average is " + average + ".");
+
+	}
+
+}
